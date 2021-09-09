3308b3b7115535531d897f05d1433b9c824b3519
ynl <2235125456@qq.com>
2021-09-09 13:31:56 +0800

    更新至50

---
+++ Effective C++/48.cpp
@@ -0,0 +1,10 @@
+//认识template元编程
+
+/*
+总结
+1.Template metaprogramming ( TMP, 模板元编程）可将工作由运行期移往编译期，
+因而得以实现早期错误侦测和更高的执行效率。
+2.TMP 可被用来生成“基千政策选择组合” ( based on combinations of policy choices ) 的客户定制代码，
+也可用来避免生成对某些特殊类型并不适合的代码 。
+
+*/
